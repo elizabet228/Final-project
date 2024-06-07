a2ef913ef6d318b135bbe1708441fbef0cffa3c9
Elizabet <liza.golemba@gmail.com>
2024-06-07 17:08:45 +0200

    Task2: Dodano funkcję do wczytywania plików JSON

--- project.py
+++ project.py
@@ -10,3 +10,17 @@ if __name__ == "__main__":
     args = parse_arguments()
     print(f"Input file: {args.input_file}")
     print(f"Output file: {args.output_file}")
+
+import json
+
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except json.JSONDecodeError as e:
+        print(f"Błąd dekodowania JSON: {e}")
+        return None
+    except FileNotFoundError:
+        print(f"Plik {file_path} nie został znaleziony.")
+        return None
